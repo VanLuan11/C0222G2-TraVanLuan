38a02968dd0cba7741d9a3b10048fd901bbde528
Your Name <travanluandn@gmail.com>
2022-03-07 22:19:00 +0700

    nop bai 7-3

---
+++ module1/ss7_bien_kieu_du_lieu_va_toan_tu/thuc_hanh/SuDungToanTu.js
@@ -0,0 +1,8 @@
+let Width;
+let Height;
+Width = prompt("Nhập chiều cao");
+Height = prompt("Nhập chiều rộng");
+let width = parseInt(Width);
+let height = parseInt(Height);
+let tong = width * height;
+document.write("Diện tích hình chữ nhật: " + tong);
