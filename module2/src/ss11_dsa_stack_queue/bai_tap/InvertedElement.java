e2d938349dfa6b29d9fb162c61730bf8d7ecedfd
Your Name <travanluandn@gmail.com>
2022-04-17 23:10:28 +0700

    nop bai

--- module2/src/ss11_dsa_stack_queue/bai_tap/InvertedElement.java
+++ module2/src/ss11_dsa_stack_queue/bai_tap/InvertedElement.java
@@ -7,19 +7,40 @@ import java.util.Stack;
 public class InvertedElement {
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<>();
-        int[] array = {1, 2, 3, 4, 5};
+        Stack<String> stack2 = new Stack<>();
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Trước khi đảo");
-        for (int number: array) {
-            System.out.print(number);
+        // đảo mảng
+//        System.out.print("nhập số: ");
+//        int number = Integer.parseInt(scanner.nextLine());
+        int[] arrayNumber = {1,2,3,4,5,6,7};
+        for (int a: arrayNumber) {
+            System.out.print(a);
+        }
+        System.out.println("---------------");
+
+        for (int i = 0; i < arrayNumber.length; i++) {
+            stack.push(arrayNumber[i]);
+        }
+
+        for (int i = 0; i < arrayNumber.length; i++) {
+            arrayNumber[i] = stack.pop();
+        }
+
+        for (int i = 0; i < arrayNumber.length; i++) {
+            System.out.print(arrayNumber[i]);
         }
         System.out.println();
-        for (int i = array.length-1; i >= 0  ; i--) {
-            stack.push(array[i]);
+
+//         đảo chuỗi
+        System.out.println("Nhập chuỗi muốn đảo: ");
+        String str = scanner.nextLine();
+        for (int i = 0; i < str.length(); i++) {
+            stack2.push(str.charAt(i) + "");
         }
         System.out.println("Sau khi đảo");
-        for (int nunber2: stack) {
-            System.out.print(nunber2);
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(stack2.pop());
 
         }
     }
