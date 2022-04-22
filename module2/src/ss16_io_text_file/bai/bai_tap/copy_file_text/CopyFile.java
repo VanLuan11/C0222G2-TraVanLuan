f233dbfbfccf7f75bbb9c134525dfdc7b6c6334a
Your Name <travanluandn@gmail.com>
2022-04-22 16:34:36 +0700

    nop bai

--- module2/src/ss16_io_text_file/bai/bai_tap/copy_file_text/CopyFile.java
+++ module2/src/ss16_io_text_file/bai/bai_tap/copy_file_text/CopyFile.java
@@ -7,20 +7,20 @@ public class CopyFile {
     public static void main(String[] args) throws IOException {
         InputStream inputStream = null;
         OutputStream outputStream = null;
-//        try {
-//            inputStream = new FileInputStream("src/ss16_io_text_file/bai/thuc_hanh/find_the_maximum_value/numbers.txt");
-//            outputStream = new FileOutputStream("src/ss16_io_text_file/bai/bai_tap/copy_file_text/file.txt");
-//            int length;
-//            byte[] buffter = new byte[1024];
-//
-//            while ((length == inputStream.read(buffter)) > 0) {
-//                outputStream.write(buffter, 0, length);
-//            }
-//            System.out.println("File is");
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } finally {
-//       inputStream.close();
-//        }
+        try {
+            inputStream = new FileInputStream(new File("src/ss16_io_text_file/bai/bai_tap/read_file_csv/file.txt"));
+            outputStream = new FileOutputStream(new File("src/ss16_io_text_file/bai/bai_tap/copy_file_text/file.txt"));
+            int length ;
+            byte[] buffer = new byte[1241];
+            while ((length = inputStream.read(buffer))> 0){
+                outputStream.write(buffer, 0,length);
+            }
+            System.out.println("");
+        }catch (IOException e ){
+            e.printStackTrace();
+        }finally {
+            inputStream.close();
+            outputStream.close();
+        }
     }
 }
