a1c7fe2f8a6357ad55f27dd2be20bcceae4acbc5
Your Name <you@example.com>
2022-06-22 22:39:21 +0700

    nop bai

--- module4/ss6_jpa/bai_tap/blog/src/main/java/com/luan/blog/repository/IBlogRepository.java
+++ module4/ss6_jpa/bai_tap/blog/src/main/java/com/luan/blog/repository/IBlogRepository.java
@@ -2,8 +2,14 @@ package com.luan.blog.repository;
 
 import com.luan.blog.model.Blog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IBlogRepository extends JpaRepository<Blog, Integer> {
+    @Query(value = " select * " +
+            " from blog ", nativeQuery = true)
+    List<Blog> findAll();
 }
