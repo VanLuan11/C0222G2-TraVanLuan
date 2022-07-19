c96ad56f36949d05ae171982a481f8891c0e6b44
Your Name <you@example.com>
2022-07-19 20:13:14 +0700

    nop bai

--- module5/angular-case-study/src/app/app.module.ts
+++ module5/angular-case-study/src/app/app.module.ts
@@ -3,22 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { ListComponent } from './facility/list/list.component';
 import { HomeComponent } from './home/home.component';
-
-
-
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { ListFacilityComponent } from './facility/list-facility/list-facility.component';
+import { CreateComponent } from './facility/create/create.component';
+import { EditComponent } from './facility/edit/edit.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     HeaderComponent,
     FooterComponent,
-    ListComponent,
-    HomeComponent,
+    ListFacilityComponent,
+    CreateComponent,
+    EditComponent,
   ],
   imports: [
     BrowserModule,
--- module5/angular-case-study/src/app/facility/edit/edit.component.spec.ts
+++ module5/angular-case-study/src/app/facility/edit/edit.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ListComponent } from './list.component';
+import { EditComponent } from './edit.component';
 
-describe('ListComponent', () => {
-  let component: ListComponent;
-  let fixture: ComponentFixture<ListComponent>;
+describe('EditComponent', () => {
+  let component: EditComponent;
+  let fixture: ComponentFixture<EditComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ListComponent ]
+      declarations: [ EditComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ListComponent);
+    fixture = TestBed.createComponent(EditComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
