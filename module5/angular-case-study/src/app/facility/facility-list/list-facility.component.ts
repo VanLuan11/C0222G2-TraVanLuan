aeae2c80d08d638ec15dafa1b82e6fe38b51bda1
Your Name <you@example.com>
2022-07-25 10:16:08 +0700

    nop bai

--- module5/angular-case-study/src/app/facility/facility-list/list-facility.component.ts
+++ module5/angular-case-study/src/app/facility/facility-list/list-facility.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Facility} from "../facility";
 
 @Component({
   selector: 'app-list-facility',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListFacilityComponent implements OnInit {
 
-  constructor() { }
+  facilities: Facility[] = [];
+
+  constructor() {
+    this.facilities.push({id: 'SV-1234', nameService: 'Ocean Outit', type: 'villa', typeRent: 'month', quantity: '2',
+      area: ' 85.8 m', facilityFree: null, numberOfFloor: '2', description: 'Mới thuê', standardRoom: 'vip', poolArea: '32.3'});
+  }
 
   ngOnInit(): void {
   }
---
+++ module5/angular-case-study/src/app/facility/facility.ts
@@ -0,0 +1,14 @@
+export interface Facility {
+  id?: string;
+  type?: string;
+  nameService?: string;
+  cost?: string;
+  typeRent?: string;
+  quantity?: string;
+  area?: string;
+  facilityFree?: string;
+  numberOfFloor?: string;
+  description?: string;
+  standardRoom?: string;
+  poolArea?: string;
+}
