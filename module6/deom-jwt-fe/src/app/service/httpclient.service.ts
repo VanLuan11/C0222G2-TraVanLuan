ad8edf6c15c109fac516ebfeb2b3c8bbdcd70345
Your Name <you@example.com>
2022-09-06 10:18:00 +0700

    up

---
+++ module6/deom-jwt-fe/src/app/service/httpclient.service.ts
@@ -0,0 +1,52 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+export class Employee {
+  constructor(
+    public empId: string,
+    public name: string,
+    public designation: string,
+    public salary: string,
+  ) {
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpClientService {
+
+  constructor(
+    private httpClient: HttpClient
+  ) {
+  }
+
+
+  getEmployees() {
+    const basicString = this.getHeaders();
+
+    const headers = new HttpHeaders(
+      {Authorization: basicString}
+    );
+    console.log('test call');
+    return this.httpClient.get<Employee[]>('http://localhost:8080/employees', {headers});
+  }
+
+  public deleteEmployee(employee) {
+    return this.httpClient.delete<Employee>('http://localhost:8080/employees' + '/' + employee.empId);
+  }
+
+  public createEmployee(employee) {
+    return this.httpClient.post<Employee>('http://localhost:8080/employees', employee);
+  }
+
+
+  getHeaders() {
+    const username = 'admin';
+    const password = 'password';
+
+    const basicString = 'Basic ' + window.btoa(username + ':' + password);
+    return basicString;
+  }
+
+}
