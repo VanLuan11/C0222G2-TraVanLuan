7a59ad85d9607887dd1ab47b88e8e6c5738b25d5
Your Name <you@example.com>
2022-09-12 16:48:41 +0700

    up

---
+++ module6/shop-online/src/app/product/detail-product/detail-product.component.ts
@@ -0,0 +1,42 @@
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from '../service/product.service';
+import {Product} from '../model/product';
+import {ActivatedRoute, ParamMap, Router} from '@angular/router';
+import {Title} from '@angular/platform-browser';
+import {ToastrService} from 'ngx-toastr';
+
+@Component({
+  selector: 'app-detail-product',
+  templateUrl: './detail-product.component.html',
+  styleUrls: ['./detail-product.component.css']
+})
+export class DetailProductComponent implements OnInit {
+  product: Product;
+
+  constructor(private productService: ProductService,
+              private toast: ToastrService,
+              private title: Title,
+              private active: ActivatedRoute,
+              private router: Router) {
+    this.title.setTitle("Chi Tiết Sản Phẩm")
+  }
+
+  ngOnInit(): void {
+    this.getById()
+  }
+
+
+  getById(){
+    this.active.paramMap.subscribe((paraMap: ParamMap) => {
+      const id = paraMap.get('id')
+      this.productService.getFindById(id).subscribe(data => {
+        // @ts-ignore
+        this.product = data;
+        if(data == null){
+          this.toast.error("Không có  sản phẩm này",'Thông Báo!')
+          this.router.navigateByUrl("/home").then()
+        }
+      })
+    })
+  }
+}
---
+++ module6/shop-online/src/app/product/lap-top-gaming/lap-top-gaming.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {Product} from '../model/product';
+import {ProductService} from '../service/product.service';
+import {ToastrService} from 'ngx-toastr';
+import {Title} from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-lap-top-gaming',
+  templateUrl: './lap-top-gaming.component.html',
+  styleUrls: ['./lap-top-gaming.component.css']
+})
+export class LapTopGamingComponent implements OnInit {
+
+  listProduct: Product[] = [];
+
+  constructor(private productService: ProductService, private toast: ToastrService,
+              private title: Title) {
+    this.title.setTitle('Tech Shop');
+  }
+
+  ngOnInit(): void {
+    this.getAllProduct();
+  }
+
+  getAllProduct() {
+    this.productService.getAllLapTopGaming().subscribe((data: Product[]) => {
+      this.listProduct = data;
+    });
+  }
+
+}
